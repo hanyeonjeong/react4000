3c19d2206863edfa81746b603bfd56752073e025
hanyeonjeong <80299203+hanyeonjeong@users.noreply.github.com>
2021-05-25 15:09:54 +0900

    2021.05.25

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function Welcome(props){
-  return <h1>Hello, {props.name}</h1>;
+function formatDate(date) {
+    return date.toLocaleDateString();
 }
 
-function App() {
-  return (
-    <div>
-      <Welcome name="han"/>
-      <Welcome name="yeon"/>
-      <Welcome name="jeong"/>
-    </div>
-  );
+function Comment(props) {
+    return (
+        <div className="comment">
+            <div className="user-info">
+            <img
+                className="avatar"
+                src={props.author.avatarUrl}
+                alt={props.author.name}
+            />
+            <div className="user-name">{props.author.name}</div>
+            </div>
+            <div className="comment-text">{props.text}</div>
+            <div className="comment-date">{formatDate(props.date)}</div>
+        </div>
+    );
 }
 
-ReactDOM.render(App(), document.getElementById("root"));
+const comment = {
+    date: new Date(),
+    text: "열심히 할수록 기회는 따른다.",
+    author: {
+    name: "hanyeonjeong",
+    avatarUrl:
+        "http://hyj981015.dothome.co.kr/portfolio/assets/img/aboutImg02@2x.jpg",
+    },
+};
+
+ReactDOM.render(
+    <Comment date={comment.date} text={comment.text} author={comment.author} />,
+    document.getElementById("root")
+);
