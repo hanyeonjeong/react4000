a903efb0d188bfc7d1abc01cdab10ff5a75c9a2e
hanyeonjeong <80299203+hanyeonjeong@users.noreply.github.com>
2021-05-26 10:15:13 +0900

    4000

--- src/index.js
+++ src/index.js
@@ -1,38 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+//함수화 시키기
+function Avata(props) {
+  return <img src={props.user.avatarUrl} alt={props.user.name} />;
+}
 function formatDate(date) {
-    return date.toLocaleDateString();
+  return date.toLocaleDateString();
 }
-
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avata user={props.user} />
+      <div>{props.user.name}</div>
+    </div>
+  );
+}
 function Comment(props) {
-    return (
-        <div className="comment">
-            <div className="user-info">
-            <img
-                className="avatar"
-                src={props.author.avatarUrl}
-                alt={props.author.name}
-            />
-            <div className="user-name">{props.author.name}</div>
-            </div>
-            <div className="comment-text">{props.text}</div>
-            <div className="comment-date">{formatDate(props.date)}</div>
-        </div>
-    );
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-text">{formatDate(props.date)}</div>
+    </div>
+  );
 }
-
 const comment = {
-    date: new Date(),
-    text: "열심히 할수록 기회는 따른다.",
-    author: {
+  date: new Date(),
+  text: "열심히 할수록 기회는 따른다.",
+  author: {
     name: "hanyeonjeong",
     avatarUrl:
-        "http://hyj981015.dothome.co.kr/portfolio/assets/img/aboutImg02@2x.jpg",
-    },
+      "http://hyj981015.dothome.co.kr/portfolio/assets/img/aboutImg02.jpg",
+  },
 };
-
 ReactDOM.render(
-    <Comment date={comment.date} text={comment.text} author={comment.author} />,
-    document.getElementById("root")
+  <Comment date={comment.date} text={comment.text} author={comment.author} />,
+  document.getElementById("root")
 );
