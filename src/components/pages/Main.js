f2f8dc06cc9016df122a733299be703d64ea3b5c
hanyeonjeong <80299203+hanyeonjeong@users.noreply.github.com>
2021-05-27 12:09:14 +0900

    400

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -2,42 +2,29 @@ import { func } from "prop-types";
 import React from "react";
 import Header from "../Header";
 import Layout from "../Layout";
-// function Main (){
-//     return(
-//         <div id="wrap">
-// <section id="mainCont">
-//     <div className="main__cont">
-//         <div>We provide</div>
-//         <div>visual Coding</div>
-//         <div>Solutions</div>
-//         <div>For you webs</div>
-//     </div>
-// </section>
-//         </div>
-//     );
-// }
-function Info({ text1, text2, text3, text4 }) {
+
+function Info({ text1 }) {
   return (
-    <div className="main__cont">
       <div>{text1}</div>
-      <div>{text2}</div>
-      <div>{text3}</div>
-      <div>{text4}</div>
-    </div>
   );
 }
+
+const textInfo = [
+  {text1="We provide"},
+  {text2="visual Coding"},
+  {text3="Solutions"},
+  {text4="For you webs"},
+]
+
 function Main() {
   return (
     <div id="wrap">
       <Header />
       <Layout>
         <section id="mainCont">
-          <Info
-            text1="We provide"
-            text2="visual Coding"
-            text3="Solutions"
-            text4="For you webs"
-          />
+        <div className="main__cont">
+         {textInfo.map((el) => (<Info text={text.text} />))}
+         </div>
         </section>
       </Layout>
     </div>
